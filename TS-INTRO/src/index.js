914eabad7f414bc6dda447d133c6ee3a20cb5f90
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-08-01 12:50:45 +0530

    union

--- TS-INTRO/src/index.js
+++ TS-INTRO/src/index.js
@@ -1,1 +1,86 @@
 console.log("hello");
+// Number
+var a = 10;
+// a = "str";
+a = 12;
+// String
+var str = "abc";
+// str = 10;
+// Boolean
+var flag = false;
+// flag = "str";
+// Array
+var array = [1, 2, 3, 4, 5];
+// let array: number[] = [1, 2, 3, "c", "b","a"]
+var array1 = [1, 2, 3, 4];
+var person = {
+    //   id: "1" if miss id then it will throw error.
+    id: 1,
+    name: "ankit",
+};
+var task1 = {
+    id: 1,
+    title: "abc",
+    status: false,
+    user: "123", // we can skip it also.
+    // user: 123,
+};
+// Create an array of products
+var products = [
+    {
+        image: "product1.jpg",
+        price: 25.99,
+        id: 1,
+        status: true,
+        colour: "red",
+    },
+    {
+        image: "product2.jpg",
+        price: 19.99,
+        id: 2,
+        status: true,
+    },
+    {
+        image: "product3.jpg",
+        price: 39.99,
+        id: 3,
+        status: false,
+        colour: "blue",
+    },
+    // Add more products here...
+];
+// Example of accessing a product
+var firstProduct = products[0];
+console.log(firstProduct.image); // Output: "product1.jpg"
+console.log(firstProduct.price); // Output: 25.99
+console.log(firstProduct.status); // Output: true
+console.log(firstProduct.colour); // Output: "red"
+var arrayOfProducts = [
+    {
+        image: "https://images.meesho.com/images/products/197070815/dkszs_512.webp",
+        price: 500,
+        id: 1,
+        status: true,
+        colour: "red",
+    },
+];
+var arrayOfProduct = [
+    {
+        id: 1,
+        image: "abc.com/jpeg",
+        price: 2999,
+        status: false,
+        colour: "Red",
+    },
+    {
+        id: 2,
+        image: "abcd.com/jpeg",
+        price: 1999,
+        status: false,
+        colour: "Blue",
+    },
+];
+// Union and Intersection
+var b = 10;
+b = "str";
+b = true;
--- TS-INTRO/src/index.ts
+++ TS-INTRO/src/index.ts
@@ -1,1 +1,153 @@
 console.log("hello");
+
+// Number
+
+let a: number = 10;
+// a = "str";
+a = 12;
+
+// String
+
+let str: string = "abc";
+// str = 10;
+
+// Boolean
+
+let flag: boolean = false;
+// flag = "str";
+
+// Array
+
+let array: number[] = [1, 2, 3, 4, 5];
+// let array: number[] = [1, 2, 3, "c", "b","a"]
+
+let array1: Array<number> = [1, 2, 3, 4];
+
+// array1.push("str")
+
+// Object *****
+
+type Data = {
+  id: number;
+  name: string;
+};
+
+let person: Data = {
+  //   id: "1" if miss id then it will throw error.
+  id: 1,
+  name: "ankit",
+};
+
+// person.address = "abc Street"
+
+type Todo = {
+  id: number;
+  title: string;
+  status: boolean;
+  user?: string; // optional
+};
+
+const task1: Todo = {
+  id: 1,
+  title: "abc",
+  status: false,
+  user: "123", // we can skip it also.
+  // user: 123,
+};
+
+// Array of Objects
+
+// Problem
+// Create an array of products with following properties
+
+// image, price, id ,status, colour(optional)
+
+// Define the Product interface
+type Product = {
+  image: string;
+  price: number;
+  id: number;
+  status: boolean;
+  colour?: string; // Optional property
+};
+
+// Create an array of products
+const products: Product[] = [
+  {
+    image: "product1.jpg",
+    price: 25.99,
+    id: 1,
+    status: true,
+    colour: "red",
+  },
+  {
+    image: "product2.jpg",
+    price: 19.99,
+    id: 2,
+    status: true,
+  },
+  {
+    image: "product3.jpg",
+    price: 39.99,
+    id: 3,
+    status: false,
+    colour: "blue",
+  },
+  // Add more products here...
+];
+
+// Example of accessing a product
+const firstProduct: Product = products[0];
+console.log(firstProduct.image); // Output: "product1.jpg"
+console.log(firstProduct.price); // Output: 25.99
+console.log(firstProduct.status); // Output: true
+console.log(firstProduct.colour); // Output: "red"
+
+type product = {
+  image: string;
+  price: number;
+  id: number;
+  status: boolean;
+  colour?: string;
+};
+
+const arrayOfProducts: Array<product> = [
+  {
+    image: "https://images.meesho.com/images/products/197070815/dkszs_512.webp",
+    price: 500,
+    id: 1,
+    status: true,
+    colour: "red",
+  },
+];
+
+type Products = {
+  id: number;
+  image: string;
+  price: number;
+  status: boolean;
+  colour?: string;
+};
+
+let arrayOfProduct: Products[] = [
+  {
+    id: 1,
+    image: "abc.com/jpeg",
+    price: 2999,
+    status: false,
+    colour: "Red",
+  },
+  {
+    id: 2,
+    image: "abcd.com/jpeg",
+    price: 1999,
+    status: false,
+    colour: "Blue",
+  },
+];
+
+// Union and Intersection
+
+let b: number | string | boolean = 10;
+b = "str";
+b = true;
